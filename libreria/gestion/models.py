a66a496de498e8391a4b7003b13314f897d155c7
Michael Elias Huamani Fuentes <85207872+Catfish30@users.noreply.github.com>
2021-09-22 13:21:42 -0500

    chore:migracion postgres

--- libreria/gestion/models.py
+++ libreria/gestion/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductoModel(models.Model):
+
+    class OpcionesUM(models.TextChoices):
+        UNIDADES = 'UN', 'UNIDADES'
+        DOCENA =  'DOC', 'DOCENA'
+        CIENTO = 'CI', 'CIENTO'
+        MILLAR = 'MI', 'MILLAR'
+
+
+    productoId = models.AutoField(primary_key=True, null=False, unique=True, db_column='id')
+
+    productoNombre = models.CharField(max_length=45, null=False, db_column='nombre')
+
+    productoPrecio = models.DecimalField(max_digits=5,decimal_places=2,db_column='precio')
+
+    productoUnidadMedida = models.TextField(choices=OpcionesUM.choices, default=OpcionesUM.UNIDADES, db_column='unidad_medida')
+
+    class Meta():
+        db_table='productos'
+        ordering=['-productoPrecio']
+        verbose_name='producto'
+        verbose_name_plural = 'productos'
+
+class ClienteModel(models.Model):
+
+    clienteId = models.AutoField(db_column='id', primary_key=True, unique=True, null=False)
+
+    clienteNombre = models.CharField(max_length=45, db_column='nombre')
+
+    clienteDocumento = models.CharField(max_length=12, db_column='documento', unique=True)
+
+    clienteDireccion = models.CharField(max_length=100, db_column='direccion')
+
+    class Meta:
+        db_table = 'clientes'
+        verbose_name = 'cliente'
+        verbose_name_plural = 'clientes'
+
+class CabeceraModel(models.Model):
+    # tipos = VENTA V | COMPRA C
+    class OpcionesTipo(models.TextChoices):
+        VENTA = 'V', 'VENTA'
+        COMPRA = 'C', 'COMPRA'
+
+    cabeceraId = models.AutoField(db_column='id', primary_key=True, unique=True, null=False)
+
+    cabeceraFecha = models.DateTimeField(auto_now_add=True, db_column='fecha')
+
+    cabeceraTipo = models.TextField(choices=OpcionesTipo.choices, db_column='tipo', null=False)
+
+    # related_name => sirve para ingresar desde la clase ClienteModel a todos sus registros de cabeceras
+    # on_delete => que accion debe tomar cuando un registro de esa FK es eliminado, sus valores pueden ser:
+    # CASCADE => elimina ese cliente y luego todas sus cabeceras
+    # PROTECT => no permite la eliminacion de los clientes siempre y cuando tengan una cabecera
+    # SET_NULL => elimina el cliente y todas sus cabeceras la cambia el valor a null
+    # DO_NOTHING => no hace nada, mantiene su valor a pesar que se elimino dicho cliente
+    # RESTRICT => restringe la eliminacion
+    # https://docs.djangoproject.com/en/3.2/topics/db/examples/
+    clientes = models.ForeignKey(to=ClienteModel, db_column='clientes_id',null=False, related_name='clienteCabeceras', on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'cabecera_operaciones'
+        verbose_name = 'cabecera'
+        verbose_name_plural = 'cabeceras'
+
+class DetalleModel(models.Model):
+
+    detalleId = models.AutoField(db_column='id', primary_key=True, unique=True, null=False)
+
+    detalleCantidad = models.IntegerField(db_column='cantidad', null=False)
+
+    detalleImporte = models.DecimalField(max_digits=5, decimal_places=2, db_column='importe', null=False)
+
+    productos = models.ForeignKey(to=ProductoModel, db_column='productos_id', on_delete=models.PROTECT, related_name='productoDetalles', null=False)
+
+    cabeceras = models.ForeignKey(to=CabeceraModel, db_column='cabecera_operaciones_id', on_delete=models.PROTECT, related_name='cabeceraDetalles', null=False)
+
+    class Meta:
+        db_table='detalle_operaciones'
+        verbose_name='detalle'
+        verbose_name_plural='detalles'
