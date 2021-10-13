0f7be0f8c99c94ce03ff4854764364bfc2721350
Michael Elias Huamani Fuentes <85207872+Catfish30@users.noreply.github.com>
2021-10-12 19:51:06 -0500

    update production

--- src/config/server.js
+++ src/config/server.js
@@ -31,10 +31,10 @@ export class Server {
         })
 
         if (process.env.NODE_ENV === "production") {
-            documentacion.host = "https://tareas-express-michael.herokuapp.com/";
+            documentacion.host = "tareas-express-michael.herokuapp.com";
             documentacion.schemes = ["https"];
           } else {
-            documentacion.host = `127.0.0.1;${this.puerto}`;
+            documentacion.host = `127.0.0.1:${this.puerto}`;
             documentacion.schemes = ["http"];
           }
 
